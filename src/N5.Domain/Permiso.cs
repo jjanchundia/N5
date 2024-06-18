51ade2157e81828d3616d8c3f3fa4a101e9c3269
Jonathan Anchundia <jjanchundia92@gmail.com>
2024-06-18 17:54:24 -0500

    Agregar archivos de proyecto.

---
+++ src/N5.Domain/Permiso.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace N5.Domain
+{
+    public class Permiso
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NombreEmpleado { get; set; }
+        public string ApellidoEmpleado { get; set; }
+        public int TipoPermiso { get; set; }
+        public DateTime FechaPermiso { get; set; }
+    }
+}
